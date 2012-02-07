e0c87edb9caaf33bb383595212990eea4845f0c4
Jason Bryer <jason@bryer.org>
2012-02-07 16:21:49 -0500

    Added PISA demo.

---
+++ demo/pisa.R
@@ -0,0 +1,74 @@
+require(multilevelPSA)
+require(party)
+data(pisana)
+data(pisanaschool)
+
+pkgdir = system.file(package='multilevelPSA')
+source(paste(pkgdir, '/demo/pisa.setup.R', sep=''))
+
+school = pisanaschool[,c('COUNTRY', "CNT", "SCHOOLID",
+						"SC02Q01", #Public (1) or private (2)
+						"STRATIO" #Student-teacher ratio 
+						 )]
+names(school) = c('COUNTRY', 'CNT', 'SCHOOLID', 'PUBPRIV', 'STRATIO')
+school$SCHOOLID = as.integer(school$SCHOOLID)
+
+student = pisana[,psa.cols]
+student$CNT = as.character(student$CNT)
+student = ddply(student, 'CNT', recodePISA, .progress='text')
+student = merge(student, school, by=c('CNT', 'SCHOOLID'), all.x=TRUE)
+student = student[!is.na(student$PUBPRIV),] #Remove rows with missing PUBPRRIV
+table(student$CNT, student$PUBPRIV, useNA='ifany')
+
+#Use conditional inference trees from the party package
+mlctree = mlpsa.ctree(student[,c(1,5:48,68)], formula=PUBPRIV ~ ., level2='CNT')
+student.party = getStrata(mlctree, student, level2='CNT')
+
+student.party$mathscore = apply(student.party[,c('PV1MATH','PV2MATH','PV3MATH','PV4MATH','PV5MATH')], 1, sum) / 5
+student.party$readscore = apply(student.party[,c('PV1READ','PV2READ','PV3READ','PV4READ','PV5READ')], 1, sum) / 5
+student.party$sciescore = apply(student.party[,c('PV1SCIE','PV2SCIE','PV3SCIE','PV4SCIE','PV5SCIE')], 1, sum) / 5
+
+#plot.tree(mlctree, psa.cols[c(4:48)], student.party$level2, colLabels=psa.cols.names)
+
+results.psa.math = mlpsa(response=student.party$mathscore, treatment=student.party$PUBPRIV, strata=student.party$strata, level2=student.party$CNT, minN=5)
+summary(results.psa.math)
+ls(results.psa.math)
+
+results.psa.math$level1.summary
+results.psa.math$level2.summary
+
+plot(results.psa.math)
+plot.mlpsa.difference(results.psa.math)
+
+
+plotcirc.multilevel.psa(results.psa.math, xlab='Public', ylab='Private', legendlab=FALSE, level1.plot=FALSE, level1.rug.plot=NULL, level1.projection.lines=FALSE) + opts(legend.position='none')
+plot.multilevel.distribution(results.psa.math)
+
+
+
+##### Experimental
+p1 = plotcirc.multilevel.psa(results.psa.math, xlab='Public', ylab='Private', legendlab=FALSE, level1.plot=FALSE, level1.rug.plot=NULL, level1.projection.lines=FALSE) + opts(legend.position='none') +
+	xlab(NULL) + ylab(NULL) #opts(legend.position=c(.88,.25)) +  scale_size_continuous('Sample Size') 
+p2 = plot.multilevel.distribution(results.psa.math, x='level2', y='Public', flip=TRUE)
+p3 = plot.multilevel.distribution(results.psa.math, x='level2', y='Private', flip=FALSE)
+
+
+grid_layout <- grid.layout(nrow=2, ncol=2, widths=c(1,2), heights=c(2,1))
+grid.newpage()
+pushViewport( viewport( layout=grid_layout ) )
+align.plots(grid_layout, list(p3, 1, 1), list(p1, 1, 2), list(p2, 2, 2))
+
+
+
+grid.arrange(p1,p2)
+
+
+
+
+l1 = results.psa.math$level1.summary
+l2 = results.psa.math$level2.summary
+l1$Panel = 'Level 1'
+l2$Panel = 'Level 2'
+l1 = l1[,c('Public', 'Private', 'Panel')]
+l2 = l2[,c('xmark', 'ymark' , 'Panel')])
+names(l2) = names(l1)
---
+++ demo/pisa.setup.R
@@ -0,0 +1,157 @@
+psa.cols = c(
+	"CNT", #Country
+	"SCHOOLID",
+	"StIDStd", #Student ID
+	"ST01Q01", #Grade
+	"ST04Q01", #Sex
+	"ST05Q01", #Attend 1=No; 2=Yes, <1 year; 3=Yes, >1 year
+	"ST06Q01", #Age
+	"ST07Q01", #Repeat 1=No, 2=Yes once; 3=Yes two or more
+	"ST08Q01", #At home mother 1=Yes; 2=No
+	"ST08Q02", #At home father 1=Yes; 2=No
+	"ST08Q03", #At home brothers 1=Yes; 2=No
+	"ST08Q04", #At home sisters
+	"ST08Q05", #At home grandparents
+	"ST08Q06", #At home others
+	"ST10Q01", #Mother highest schooling
+	"ST12Q01", #Mother current job status 1=Full-time; 2=Part-time; 3=Looking for work; 4=Other
+	"ST14Q01", #Father highest schooling
+	"ST16Q01", #Father current job status
+	"ST19Q01", #Language at home 1=Language of test; 2=Another language
+	"ST20Q01", #Desk
+	"ST20Q02", #Own room
+	"ST20Q03", #Study place
+	"ST20Q04", #Computer
+	"ST20Q05", #Software
+	"ST20Q06", #Internet
+	"ST20Q07", #Literature
+	"ST20Q08", #Poetry
+	"ST20Q09", #Art
+	"ST20Q10", #Textbooks
+	"ST20Q12", #Dictionary
+	"ST20Q13", #Dishwasher
+	"ST20Q14", #DVD
+	"ST21Q01", #How many cellphones
+	"ST21Q02", #How may TVs
+	"ST21Q03", #How many computers
+	"ST21Q04", #How many cars
+	"ST21Q05", #How many rooms bath or shower
+	"ST22Q01", #How many books 1=0-10; 2=11-25; 3=26-100; 4=101-200; 5=201-500; 6= >500
+	"ST23Q01", #Reading enjoyment time 1=Don't read; 2= <30 minutes/day; 3=1-2 hours/day; 4= >2hours/day
+	"ST31Q01", #Enrich in test language
+	"ST31Q02", #Enrich in mathematics
+	"ST31Q03", #Enrich in science
+	"ST31Q05", #Remedial in test language
+	"ST31Q06", #Remedial in mathematics
+	"ST31Q07", #Remedial in science
+	"ST32Q01", #Out of school lessons in test language
+	"ST32Q02", #Out of school lessons <maths> 1	Do not attend 2	Less than 2 hours a week 3	2 up to 4 Hours a week 4	4 up to 6 hours per week 5	6 or more hours a week 7	N/A 8	Invalid 9	Miss
+	"ST32Q03", #Out of school lessons in science
+	"PV1MATH",    "PV2MATH",    "PV3MATH",    "PV4MATH",    "PV5MATH", #Math scores
+	"PV1READ",    "PV2READ",    "PV3READ",    "PV4READ",    "PV5READ", #Reading scores
+	"PV1SCIE",    "PV2SCIE",    "PV3SCIE",    "PV4SCIE",    "PV5SCIE" #Science scores
+)
+
+
+psa.cols.names = as.data.frame(matrix(c(
+	"CNT",      'CNT',     "Country",
+	"SCHOOLID", 'SchoolId', "SchoolID",
+	"StIDStd", 'StudentId', "Student ID",
+	"ST01Q01", 'Grade',   "Grade",
+	"ST04Q01", 'Sex',     "Sex",
+	"ST05Q01", 'Attend',  "Attend",
+	"ST06Q01", 'Age',     "Age",
+	"ST07Q01", 'Repeat',  "Repeat",
+	"ST08Q01", 'Mother',  "At home mother",
+	"ST08Q02", 'Father',  "At home father",
+	"ST08Q03", 'Brother', "At home brothers",
+	"ST08Q04", 'Sister',  "At home sisters",
+	"ST08Q05", 'GrandPa', "At home grandparents",
+	"ST08Q06", 'Other',   "At home others",
+	"ST10Q01", 'MomEd',   "Mother highest schooling",
+	"ST12Q01", 'MomJob',  "Mother current job status",
+	"ST14Q01", 'DadEd',   "Father highest schooling",
+	"ST16Q01", 'DadJob',  "Father current job status",
+	"ST19Q01", 'Lang',    "Language at home",
+	"ST20Q01", 'Desk',    "Desk",
+	"ST20Q02", 'OwnRoom', "Own room",
+	"ST20Q03", 'StudyPl', "Study place",
+	"ST20Q04", 'Computer',"Computer",
+	"ST20Q05", 'Software',"Software",
+	"ST20Q06", 'Internet',"Internet",
+	"ST20Q07", 'Lit',     "Literature",
+	"ST20Q08", 'Poetry',  "Poetry",
+	"ST20Q09", 'Art',     "Art",
+	"ST20Q10", 'TxtBooks',"Textbooks",
+	"ST20Q12", 'Dict',    "Dictionary",
+	"ST20Q13", 'DishW',   "Dishwasher",
+	"ST20Q14", 'DVD',     "DVD",
+	"ST21Q01", 'CellPh',  "How many cellphones",
+	"ST21Q02", 'TVs',     "How many TVs",
+	"ST21Q03", 'nComp',   "How many computers",
+	"ST21Q04", 'nCars',   "How many cars",
+	"ST21Q05", 'nBaths',  "How many rooms bath or shower",
+	"ST22Q01", 'nBooks',  "How many books",
+	"ST23Q01", 'Reading',      "Reading enjoyment time",
+	"ST31Q01", 'EnrichLang',   "Enrich in test language",
+	"ST31Q02", 'EnrichMath',   "Enrich in mathematics",
+	"ST31Q03", 'EnrichScie',   "Enrich in science",
+	"ST31Q05", 'RemedialLang', "Remedial in test language",
+	"ST31Q06", 'RemedialMath', "Remedial in mathematics",
+	"ST31Q07", 'RemedialScie', "Remedial in science",
+	"ST32Q01", 'LangLessons',  "Out of school lessons in test language",
+	"ST32Q02", 'MathLessons',  "Out of school lessons maths",
+	"ST32Q03", 'ScieLessons',  "Out of school lessons in science"
+), ncol=3, byrow=TRUE))
+names(psa.cols.names) = c('Variable', 'ShortDesc', 'Desc')
+
+recodePISA <- function(x) {
+	x[,"ST04Q01"] = factor(x[,'ST04Q01'], levels=c(1,2), labels=c('Female', 'Male')) #Sex
+	x[,"ST05Q01"] = factor(x[,'ST05Q01'], levels=c(1,2,3), labels=c('No', 'Yes <1 yr', 'Yes >1 yr')) #Attend
+	x[,"ST07Q01"] = factor(x[,'ST07Q01'], levels=c(1,2,3), labels=c('No', 'Once', '2 or more')) #Repeat
+	x[,"ST08Q01"] = factor(x[,'ST08Q01'], levels=c(1,2), labels=c('Yes', 'No')) #At home mother 1=Yes; 2=No
+	x[,"ST08Q02"] = factor(x[,'ST08Q02'], levels=c(1,2), labels=c('Yes', 'No')) #At home father 1=Yes; 2=No
+	x[,"ST08Q03"] = factor(x[,'ST08Q03'], levels=c(1,2), labels=c('Yes', 'No')) #At home brothers 1=Yes; 2=No
+	x[,"ST08Q04"] = factor(x[,'ST08Q04'], levels=c(1,2), labels=c('Yes', 'No')) #At home sisters
+	x[,"ST08Q05"] = factor(x[,'ST08Q05'], levels=c(1,2), labels=c('Yes', 'No')) #At home grandparents
+	x[,"ST08Q06"] = factor(x[,'ST08Q06'], levels=c(1,2), labels=c('Yes', 'No')) #At home others
+	x[,"ST10Q01"] = as.integer(x[,'ST10Q01']) #Mother highest schooling
+	x[,"ST12Q01"] = factor(x[,'ST12Q01'], levels=c(1,2,3,4), labels=c('Full-time','Part-Time','Looking','Other')) #Mother current job status
+	x[,"ST14Q01"] = as.integer(x[,'ST14Q01']) #Father highest schooling
+	x[,"ST16Q01"] = factor(x[,'ST16Q01'], levels=c(1,2,3,4), labels=c('Full-time','Part-Time','Looking','Other')) #Father current job status
+	x[,"ST19Q01"] = factor(x[,'ST19Q01'], levels=c(1,2), labels=c('Test Lang', 'Other Lang')) #Language at home
+	x[,"ST20Q01"] = factor(x[,'ST20Q01'], levels=c(1,2), labels=c('Yes', 'No')) #Desk
+	x[,"ST20Q02"] = factor(x[,'ST20Q02'], levels=c(1,2), labels=c('Yes', 'No')) #Own room
+	x[,"ST20Q03"] = factor(x[,'ST20Q03'], levels=c(1,2), labels=c('Yes', 'No')) #Study place
+	x[,"ST20Q04"] = factor(x[,'ST20Q04'], levels=c(1,2), labels=c('Yes', 'No')) #Computer
+	x[,"ST20Q05"] = factor(x[,'ST20Q05'], levels=c(1,2), labels=c('Yes', 'No')) #Software
+	x[,"ST20Q06"] = factor(x[,'ST20Q06'], levels=c(1,2), labels=c('Yes', 'No')) #Internet
+	x[,"ST20Q07"] = factor(x[,'ST20Q07'], levels=c(1,2), labels=c('Yes', 'No')) #Literature
+	x[,"ST20Q08"] = factor(x[,'ST20Q08'], levels=c(1,2), labels=c('Yes', 'No')) #Poetry
+	x[,"ST20Q09"] = factor(x[,'ST20Q09'], levels=c(1,2), labels=c('Yes', 'No')) #Art
+	x[,"ST20Q10"] = factor(x[,'ST20Q10'], levels=c(1,2), labels=c('Yes', 'No')) #Textbooks
+	x[,"ST20Q12"] = factor(x[,'ST20Q12'], levels=c(1,2), labels=c('Yes', 'No')) #Dictionary
+	x[,"ST20Q13"] = factor(x[,'ST20Q13'], levels=c(1,2), labels=c('Yes', 'No')) #Dishwasher
+	x[,"ST20Q14"] = factor(x[,'ST20Q14'], levels=c(1,2), labels=c('Yes', 'No')) #DVD
+	x[,"ST21Q01"] = as.integer(x[,'ST21Q01']) #How many cellphones
+	x[,"ST21Q02"] = as.integer(x[,"ST21Q02"]) #How may TVs
+	x[,"ST21Q03"] = as.integer(x[,"ST21Q03"]) #How many computers
+	x[,"ST21Q04"] = as.integer(x[,"ST21Q04"]) #How many cars
+	x[,"ST23Q01"] = as.integer(x[,"ST23Q01"]) #How many rooms bath or shower
+	x[,"ST23Q01"] = as.integer(x[,"ST23Q01"]) #How many books 1=0-10; 2=11-25; 3=26-100; 4=101-200; 5=201-500; 6= >500
+	x[,"ST23Q01"] = as.integer(x[,"ST23Q01"]) #Reading enjoyment time 1=Don't read; 2= <30 minutes/day; 3=1-2 hours/day; 4= >2hours/day
+	x[,"ST31Q01"] = factor(x[,'ST31Q01'], levels=c(1,2), labels=c('Yes', 'No')) #Enrich in test language
+	x[,"ST31Q02"] = factor(x[,'ST31Q02'], levels=c(1,2), labels=c('Yes', 'No')) #Enrich in mathematics
+	x[,"ST31Q03"] = factor(x[,'ST31Q03'], levels=c(1,2), labels=c('Yes', 'No')) #Enrich in science
+	x[,"ST31Q05"] = factor(x[,'ST31Q05'], levels=c(1,2), labels=c('Yes', 'No')) #Remedial in test language
+	x[,"ST31Q06"] = factor(x[,'ST31Q06'], levels=c(1,2), labels=c('Yes', 'No')) #Remedial in mathematics
+	x[,"ST31Q07"] = factor(x[,'ST31Q07'], levels=c(1,2), labels=c('Yes', 'No')) #Remedial in science
+	x[,"ST32Q01"] = factor(x[,'ST32Q01'], levels=c(1,2,3,4,5), labels=c('Do not attend', '<2 hrs/wk', '2-4 hrs/wk', '4-6 hrs/wk', '>6 hrs/wk'))  #Out of school lessons in test language
+	x[,"ST32Q02"] = factor(x[,'ST32Q02'], levels=c(1,2,3,4,5), labels=c('Do not attend', '<2 hrs/wk', '2-4 hrs/wk', '4-6 hrs/wk', '>6 hrs/wk')) #Out of school lessons <maths>
+	x[,"ST32Q03"] = factor(x[,'ST32Q03'], levels=c(1,2,3,4,5), labels=c('Do not attend', '<2 hrs/wk', '2-4 hrs/wk', '4-6 hrs/wk', '>6 hrs/wk')) #Out of school lessons <science>
+	
+	x$mathscore = apply(x[,c("PV1MATH", "PV2MATH", "PV3MATH", "PV4MATH", "PV5MATH")], 1, sum) / 5 #Math scores
+	x$readscore = apply(x[,c("PV1READ", "PV2READ", "PV3READ", "PV4READ", "PV5READ")], 1, sum) / 5 #Reading scores
+	x$sciescore = apply(x[,c("PV1SCIE", "PV2SCIE", "PV3SCIE", "PV4SCIE", "PV5SCIE")], 1, sum) / 5 #Science scores
+	return(x)
+}
