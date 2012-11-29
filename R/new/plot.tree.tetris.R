1e4cff555ad912b3b25bb2c12f7eee64de2c8976
Jason Bryer <jason@bryer.org>
2012-11-29 12:22:17 -0500

    Renamed directory to capital R

---
+++ R/new/plot.tree.tetris.R
@@ -0,0 +1,54 @@
+#' 
+#' 
+#' TODO: Need more details
+#' 
+#' @return a ggplot2 expression
+#' @export plot.tree.tetris
+plot.tree.tetris <- function(trees, colNames, level2Col, colLabels=NULL) {
+	ncol = length(colNames) + 1
+	tree.df <- as.data.frame(matrix(nrow=length(unique(level2Col)), ncol=ncol))
+	names(tree.df) = c('level2', colNames)
+	tree.df[,2:ncol(tree.df)] = as.numeric(NA)
+	tree.df[,1] = unique(level2Col)
+	
+	convertTree <- function(tree.df.row, node, depth, tree.party) {
+		nodeVal = nodes(tree.party, node)[[1]]
+		varName = as.character(nodeVal$psplit)[7]
+		if(nodeVal$terminal) {
+			#Do nothing
+		} else {
+			tree.df.row[varName] = min(depth, tree.df.row[varName][1,1], na.rm=TRUE)
+			tree.df.row = convertTree(tree.df.row, nodes(tree.party, node)[[1]]$left[[1]], depth+1, tree.party)
+			tree.df.row = convertTree(tree.df.row, nodes(tree.party, node)[[1]]$right[[1]], depth+1, tree.party)
+		}
+		tree.df.row
+	}
+	
+	for(i in names(trees)) {
+		tree.df[which(tree.df[,1] == i),] = convertTree(tree.df[which(tree.df[,1] == i),], 1, 1, trees[[i]])
+	}
+	
+	tree.df.m <- melt(tree.df[which(tree.df$level2 %in% unique(level2Col)),], id='level2')
+	tree.df.m$level2 = as.factor(as.character(tree.df.m$level2))
+	descColName = 'variable'
+	if(!is.null(colLabels)) {
+		tree.df.m = merge(tree.df.m, colLabels, by.x='variable', by.y=names(colLabels)[1], all.x=TRUE)
+		descColName = names(colLabels)[2]
+	}
+	#Labels
+	value.freq = as.data.frame(table(tree.df.m[!is.na(tree.df.m$value),]$variable))
+	level2.freq = as.data.frame(table(tree.df.m[!is.na(tree.df.m$value),]$level2))
+	tree.df.m = merge(tree.df.m, level2.freq, by.x='level2', by.y='Var1', all.x=TRUE)
+	tree.df.m = merge(tree.df.m, value.freq, by.x='variable', by.y='Var1', all.x=TRUE)
+	#names(tree.df.m)[5:6] = c('level2.Freq', 'Var.Freq')
+	names(tree.df.m)[4:5] = c('level2.Freq', 'Var.Freq')
+	tree.df.m$Desc = as.character(tree.df.m[,descColName])
+	tree.df.m$level2 = as.character(tree.df.m$level2)
+	
+	p = ggplot(tree.df.m, aes(level2, Desc)) + geom_text(data=tree.df.m[!duplicated(tree.df.m$level2),], aes(x=level2, y='', label=level2.Freq), size=2, angle=-90, hjust=.5) +
+			geom_text(data=tree.df.m[!duplicated(tree.df.m$variable),], aes(y=Desc, x='', label=Var.Freq), size=2, hjust=.5) +
+			geom_tile(aes(fill = value)) + scale_fill_gradient("Depth", high = "lightgrey", low = "steelblue") + 
+			opts(axis.text.y=theme_text(size=6, hjust=0, vjust=.5), axis.text.x=theme_text(size=6, angle=-90, hjust=0, vjust=.5), axis.ticks=theme_blank()) + 
+			xlab(NULL) + ylab(NULL)
+	return(p)
+}
---
+++ R/plot.mlpsa.difference.R
@@ -0,0 +1,105 @@
+#' Creates a graphic summarizing the differences between treatment and comparison
+#' groups within and across level two clusters.
+#'
+#' @param x the results of \code{\link{mlpsa}}.
+#' @param xlab label for the x-axis, or NULL to exclude.
+#' @param ylab label for the y-aixs, or NULL to exclude.
+#' @param title title of the figure, or NULL to exclude.
+#' @param overall.col the color of the overall results line.
+#' @param overall.ci.col the color of the overall confidence interval.
+#' @param level2.point.size the point size of level 2 points.
+#' @param level1.points logical value indicating whether level 1 strata should be plotted.
+#' @param errorbars logical value indicating whether error bars should be plotted for
+#'        for each level 1.
+#' @param level2.rug.plot logical value indicating whether a rug plot should be
+#'        plotted for level 2.
+#' @param jitter logical value indicating whether level 1 points should be jittered.
+#' @param reorder logical value indicating whether the level two clusters should be
+#'        reordered from largest difference to smallest.
+#' @param labelLevel2 logical value indicating whether the difference for each level 2
+#'        should be labeled.
+#' @param sd If specified, effect sizes will be plotted instead of difference in the
+#'        native unit.
+#' @param ... currently unused.
+#' @export
+plot.mlpsa.difference <- function(x,
+		xlab='Difference Score', 
+		ylab=NULL,
+		title=NULL,
+		overall.col="blue",
+		overall.ci.col='green',
+		level2.point.size=NULL,
+		level1.points=TRUE,
+		errorbars=TRUE,
+		level2.rug.plot=TRUE,
+		jitter=TRUE,
+		reorder=TRUE,
+		labelLevel2=TRUE,
+		sd=NULL,
+		...
+) {
+	stopifnot(is.mlpsa(x))
+	multilevelPSA = x
+	#ggplot.alpha <- function(...) get("alpha", grep("package:ggplot2$", search()))(...)
+
+	if(missing(multilevelPSA)) {
+		stop('Must provide multilevelPSA from multilevel.psa')
+	}
+	
+	if(reorder) {
+		multilevelPSA$level2.summary = multilevelPSA$level2.summary[
+			order(multilevelPSA$level2.summary$diffwtd),]
+		ord.level2 = multilevelPSA$level2.summary$level2[
+			order(multilevelPSA$level2.summary$diffwtd)]
+		multilevelPSA$level1.summary$level2 = factor(multilevelPSA$level1.summary$level2, 
+													 levels=ord.level2)
+		multilevelPSA$level2.summary$level2 = factor(multilevelPSA$level2.summary$level2, 
+													 levels=ord.level2)
+	}
+
+	if(!is.null(sd)) {
+		multilevelPSA$level1.summary$Diff = multilevelPSA$level1.summary$Diff / sd
+		multilevelPSA$level2.summary$diffwtd = multilevelPSA$level2.summary$diffwtd / sd
+		multilevelPSA$level2.summary$ci.min = multilevelPSA$level2.summary$ci.min / sd
+		multilevelPSA$level2.summary$ci.max = multilevelPSA$level2.summary$ci.max / sd
+		multilevelPSA$overall.ci = multilevelPSA$overall.ci / sd
+		multilevelPSA$overall.wtd = multilevelPSA$overall.wtd /sd
+		multilevelPSA$plot.range = multilevelPSA$plot.range / sd
+	}
+	
+	p = ggplot(multilevelPSA$level1.summary, aes(x=level2, y=Diff)) + coord_flip() +
+			geom_hline(aes(y=0), colour='black', size=1, alpha=.7) +
+			geom_hline(yintercept=multilevelPSA$overall.wtd, colour=overall.col, size=1) + 
+			geom_hline(yintercept=multilevelPSA$overall.ci, colour=overall.ci.col, size=1) + 
+			theme(axis.ticks.margin=unit(0, "cm"), axis.text.y=element_text(size=8, angle=0, hjust=.5))
+	if(errorbars) {
+		p = p + geom_errorbar(data=multilevelPSA$level2.summary, 
+							  aes(x=level2, y=NULL, ymin=ci.min, ymax=ci.max), 
+							  colour='green', alpha=.6)
+	}
+	if(level1.points) {
+		if(jitter) {
+			p = p + geom_point(stat='identity', alpha=.3, size=.8, position='jitter')
+		} else {
+			p = p + geom_point(stat='identity', alpha=.3, size=.8)
+		}
+	}
+	p = p + geom_point(data=multilevelPSA$level2.summary, aes(x=level2, y=diffwtd, size=n), 
+					   fill='blue', alpha=.6, stat='identity', shape=21, colour='black')
+	if(level2.rug.plot) {
+		p = p + geom_rug(data=multilevelPSA$level2.summary, aes(x=NULL, y=diffwtd), 
+						 alpha=.6, size=.5, colour='blue')
+	}
+	p = p + xlab(ylab) + ylab(xlab) + scale_size_continuous('Size')
+	if(!is.null(title)) {
+		p = p + opts(title=title)
+	}
+	
+	if(labelLevel2) {
+		p = p + geom_text(data=multilevelPSA$level2.summary, aes(x=level2, 
+						label=prettyNum(diffwtd, digits=2, drop0trailing=FALSE)), 
+						y=(min(multilevelPSA$level2.summary$ci.min)), size=3, hjust=1)
+	}
+			
+	return(p)
+}
