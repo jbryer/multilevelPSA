47d6eef0c4850a88083b6b1f54a0b5f8f3cc3682
Jason Bryer <jbryer@albany.edu>
2013-02-15 07:52:51 -0500

    Updated to reflect changes in the blog post.

--- demo/pisa.R
+++ demo/pisa.R
@@ -5,12 +5,16 @@ data(pisa.colnames)
 data(pisa.psa.cols)
 
 student <- NULL
-if(FALSE & require(pisa, quietly=TRUE)) {
+if(require(pisa, quietly=TRUE)) {
 	#If the full PISA dataset is available vis-a-vis the pisa package.
 	#See http://jason.bryer.org/pisa for more information.
 	data(pisa.student)
 	data(pisa.school)
-	student = pisa.student[,psa.cols]
+	student = pisa.student[,c('CNT', 'SCHOOLID',
+							  paste0('PV', 1:5, 'MATH'),
+							  paste0('PV', 1:5, 'READ'),
+							  paste0('PV', 1:5, 'SCIE'),
+							  pisa.psa.cols)]
 	school = pisa.school[,c('COUNTRY', "CNT", "SCHOOLID",
 							"SC02Q01", #Public (1) or private (2)
 							"STRATIO" #Student-teacher ratio 
@@ -23,6 +27,9 @@ if(FALSE & require(pisa, quietly=TRUE)) {
 	student$CNT = as.character(student$CNT)
 	student = merge(student, school, by=c('CNT', 'SCHOOLID'), all.x=TRUE)
 	student = student[!is.na(student$PUBPRIV),] #Remove rows with missing PUBPRRIV
+	
+	rm(pisa.student)
+	rm(pisa.school)
 } else {
 	data(pisana)
 	student = pisana
@@ -38,7 +45,7 @@ mlctree = mlpsa.ctree(student[,c('CNT','PUBPRIV',pisa.psa.cols)],
 student.party = getStrata(mlctree, student, level2='CNT')
 
 #Tree heat map showing relative importance of covariates used in each tree.
-tree.plot(mlctree, level2Col=student$CNT)
+tree.plot(mlctree, level2Col=student$CNT, colLabels=pisa.colnames[,c('Variable','ShortDesc')])
 
 #NOTE: This is not entirely correct but is sufficient for visualization purposes.
 #See mitools package for combining multiple plausible values.
@@ -46,25 +53,29 @@ student.party$mathscore = apply(student.party[,paste0('PV', 1:5, 'MATH')], 1, su
 student.party$readscore = apply(student.party[,paste0('PV', 1:5, 'READ')], 1, sum) / 5
 student.party$sciescore = apply(student.party[,paste0('PV', 1:5, 'SCIE')], 1, sum) / 5
 
+data(pisa.countries)
+student.party <- merge(student.party, pisa.countries, by.x='CNT', by.y='Country', all.x=TRUE)
+
 results.psa.math = mlpsa(response=student.party$mathscore, 
 						 treatment=student.party$PUBPRIV, 
 						 strata=student.party$strata, 
-						 level2=student.party$CNT, minN=5)
+						 level2=student.party$CNT3, minN=5)
 summary(results.psa.math)
 ls(results.psa.math)
 
+results.psa.math$level2.summary[,c('level2','n','Private','Private.n','Public','Public.n',
+								   'diffwtd','ci.min','ci.max','df')]
 results.psa.math$overall.ci
 
-results.psa.math$level1.summary
+View(results.psa.math$level1.summary)
 View(results.psa.math$level2.summary)
 
 # These are the two main plots
 plot(results.psa.math)
-mlpsa.difference.plot(results.psa.math,
-					  sd=mean(student.party$mathscore, na.rm=TRUE))
+mlpsa.difference.plot(results.psa.math, sd=mean(student.party$mathscore, na.rm=TRUE))
 
 # Or the individual components of the main plot separately
-mlpsa.circ.plot(results.psa.math, legendlab=FALSE) #+ opts(legend.position='none')
+mlpsa.circ.plot(results.psa.math, legendlab=FALSE)
 mlpsa.distribution.plot(results.psa.math, 'Public')
 mlpsa.distribution.plot(results.psa.math, 'Private')
 
