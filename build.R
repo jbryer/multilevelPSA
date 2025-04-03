ed96130414420063706f9b839ff9d76aa3ec4e4b
Jason Bryer <jason@bryer.org>
2025-04-03 12:47:16 -0400

    Updated logo.

--- build.R
+++ build.R
@@ -22,19 +22,6 @@ source('demo/pisa.R')
 # data("pisa.psa.cols", package = 'multilevelPSA')
 # student <- pisana
 
-# Estimate the propensity scores
-mlctree <- mlpsa.ctree(student[,c('CNT','PUBPRIV',pisa.psa.cols)], 
-					   formula = PUBPRIV ~ .,
-					   level2 = 'CNT')
-# Get the strata
-student.party <- getStrata(mlctree, student, level2 = 'CNT')
-# Create a total math score as the average of the five imputed scores
-student.party$mathscore <- apply(student.party[,paste0('PV', 1:5, 'MATH')], 1, sum) / 5
-# Estimate the causal effects
-results.psa.math <- mlpsa(response=student.party$mathscore, 
-						  treatment=student.party$PUBPRIV, 
-						  strata=student.party$strata, 
-						  level2=student.party$CNT, minN=5)
 p <- mlpsa.circ.plot(
 		results.psa.math,
 		legendlab = FALSE,
@@ -54,9 +41,10 @@ hexSticker::sticker(
 	subplot = p,
 	s_width = 3,
 	s_height = 3,
-	p_size = 10,
-	p_y = 1.62,
-	p_color = '#000000',
+	p_size = 9,
+	p_x = 0.60,
+	p_y = 1.42,
+	p_color = '#00A5FF',
 	url = 'jbryer.github.io/multilevelPSA',
 	u_size = 5,
 	u_color = '#00B5EE',
